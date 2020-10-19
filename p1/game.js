6b4a72fb593e830d35c0614f0976500b117731ec
Sam Janvey <sjanvey@gmail.com>
2020-10-19 11:37:44 -0400

    Component is working and round detail being tracked

--- p1/game.js
+++ p1/game.js
@@ -1,13 +1,35 @@
+Vue.component('round-detail', {
+    data() {
+        return {
+            
+        }
+    },
+    props: {
+        'number': {
+            type: Number,
+            default: 0
+        },
+        'winner': {
+            type: String,
+            default: ''
+        }
+    },
+    template: '#round-detail',
+});
+
 const app = new Vue({
     el: '#app',
-    data: {
-        choice: null,
-        randomNumber: null,
-        round: 0,
-        playerScore: 0,
-        compScore: 0,
-        feedback: false,
-        playerWon: null
+    data() {
+        return {
+            choice: null,
+            randomNumber: null,
+            round: 0,
+            playerScore: 0,
+            compScore: 0,
+            feedback: false,
+            playerWon: null,
+            rounds: []
+        }   
     },
     methods: {
         guess() {
@@ -15,14 +37,24 @@ const app = new Vue({
             if (this.randomNumber == this.choice) {
                 this.playerScore++;
                 this.playerWon = true;
+                winner = 'Player';
             } else {
                 this.compScore++;
                 this.playerWon = false;
+                winner = 'Dealer'
             }
             this.feedback = true;
+
+            // Add Round Detail to History
+            this.rounds.push({
+                number: this.round,
+                winner: winner,
+                choice: this.choice,
+                actual: this.randomNumber
+            });
         },
         getRandomNumber() {
-            return Math.floor(Math.random() * 10);
+            return Math.floor(Math.random() * 10) + 1;
         },
         reset() {
             this.choice = '';
