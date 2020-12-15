60db6f9dc82aeffc5ab7beceeccd6dcd41e9f036
Sam Janvey <sjanvey@gmail.com>
2020-12-15 16:01:15 -0500

    Update app.js

--- p3/forgetmenot/src/app.js
+++ p3/forgetmenot/src/app.js
@@ -1,5 +1,5 @@
 export const axios = require('axios').create({
-    baseURL: process.env.VUE_APP_API_URL ?? 'https://e28-api.samjanvey.com',
+    baseURL: process.env.VUE_APP_API_URL ?? 'http://e28-api.samjanvey.loc',
     responseType: 'json',
     withCredentials: true,
 })
