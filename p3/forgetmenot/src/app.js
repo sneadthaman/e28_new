3fc04a8d11b7e188b51fbd8e1ee0379a6cd80431
Sam Janvey <sjanvey@gmail.com>
2020-12-15 15:48:47 -0500

    fixed api url

--- p3/forgetmenot/src/app.js
+++ p3/forgetmenot/src/app.js
@@ -1,5 +1,5 @@
 export const axios = require('axios').create({
-    baseURL: process.env.VUE_APP_API_URL ?? 'http://e28-api.samjanvey.loc',
+    baseURL: process.env.VUE_APP_API_URL ?? 'http://e28-api.samjanvey.com',
     responseType: 'json',
     withCredentials: true,
 })
